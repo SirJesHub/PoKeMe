159f7b2a36c896002dd1bcd073d4ca0dba299eb8
SirJesHub <siraprop.jes@gmail.com>
2022-11-01 20:39:32 +0700

    Initial setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PoKeME</h1>
+    </div>
+  );
+}
+
+export default App;
